6669fd81b1a24f529c0330b70bebdc3076c19cf8
bluecrayon52 <nathanclaymusic@gmail.com>
2016-03-01 12:38:26 -0500

    called an instance of VendingMachine

---
+++ MyVend.java
@@ -0,0 +1,17 @@
+/*  
+ * Author: Nathaniel Clay Arnold
+ * Program 3 - MyVend 
+ * CSC230-02 Spring 2016
+ */
+
+package vendingmachine; 
+
+public class MyVend{ 
+    
+    public static void main(String[] args) {
+        
+   // instantiate VendingMachine Object 
+   VendingMachine test = new VendingMachine(); 
+    
+    }
+}
